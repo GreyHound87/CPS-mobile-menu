a62d3bfee2a8e5854e01e25df797a06d485b040b
Sergei Smirnov <sasmirnov87@gmail.com>
2023-06-20 23:13:35 +0300

    menu built in

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,131 @@
-const burgerButton = document.getElementById('burgerButton');
-burgerButton.addEventListener('click', function() {
-  if (burgerButton.classList.contains('active')) {
-    burgerButton.classList.remove('active');
+function toggleBurgerButton() {
+  var menu = document.getElementById("menu");
+  var menuLogoWrapper = document.getElementById("menuLogoWrapper");
+  var burgerButton = document.getElementById("burgerButton");
+  var logoLink = document.getElementById("logoLink");
+  var logoImg = document.getElementById("logoImg");
+  var searchButton = document.getElementById("searchButton");
+  var navigation = document.getElementById("navigation");
+  var navigationList = document.getElementById("navigationList");
+  var navItem1 = document.getElementById("navItem1");
+  var navLink1 = document.getElementById("navLink1");
+  var navItem2 = document.getElementById("navItem2");
+  var navLink2 = document.getElementById("navLink2");
+  var navItem3 = document.getElementById("navItem3");
+  var navLink3 = document.getElementById("navLink3");
+  var navItem4 = document.getElementById("navItem4");
+  var navLink4 = document.getElementById("navLink4");
+  var navItem5 = document.getElementById("navItem5");
+  var navLink5 = document.getElementById("navLink5");
+  var navItem6 = document.getElementById("navItem6");
+  var navLink6 = document.getElementById("navLink6");
+  var navItem7 = document.getElementById("navItem7");
+  var navLink7 = document.getElementById("navLink7");
+  var navItem8 = document.getElementById("navItem8");
+  var navLink8 = document.getElementById("navLink8");
+  var modBtnWrapper = document.getElementById("modBtnWrapper");
+  var callButton = document.getElementById("callButton");
+  var chatButton = document.getElementById("chatButton");
+  var profileButton = document.getElementById("profileButton");
+  var mailtoLink = document.getElementById("mailtoLink");
+  var calltoLink = document.getElementById("calltoLink");
+  var langWrapper = document.getElementById("langWrapper");
+  var langLinkRU = document.getElementById("langLinkRU");
+  var langLinkEN = document.getElementById("langLinkEN");
+  var langLinkCH = document.getElementById("langLinkCH");
+  var servicesFirstLineWrapper = document.getElementById("servicesBtnWrapper");
+  var upenderException = document.getElementById("upenderException");
+  var placeholder = document.getElementById("placeholder");
+  var upenderBlurOverlay = document.getElementById("upenderBlurOverlay");
+  var isButtonActive = burgerButton.classList.contains("menu__button--active");
+
+  if (isButtonActive) {
+    burgerButton.classList.remove("menu__button--active");
+    menuLogoWrapper.classList.remove("menu__wrapper--active");
+    logoLink.classList.remove("menu__logo--active");
+    logoImg.classList.remove("logo__img--active");
+    searchButton.classList.remove("menu__button--active");
+    navigation.classList.remove("menu__navigation--active");
+    navigationList.classList.remove("navigation__list--active");
+    navItem1.classList.remove("navigation__item--active");
+    navLink1.classList.remove("navigation__link-btn--active");
+    navItem2.classList.remove("navigation__item--active");
+    navLink2.classList.remove("navigation__link-btn--active");
+    navItem3.classList.remove("navigation__item--active");
+    navLink3.classList.remove("navigation__link-btn--active");
+    navItem4.classList.remove("navigation__item--active");
+    navLink4.classList.remove("navigation__link-btn--active");
+    navItem5.classList.remove("navigation__item--active");
+    navLink5.classList.remove("navigation__link-btn--active");
+    navItem6.classList.remove("navigation__item--active");
+    navLink6.classList.remove("navigation__link-btn--active");
+    navItem7.classList.remove("navigation__item--active");
+    navLink7.classList.remove("navigation__link-btn--active");
+    navItem8.classList.remove("navigation__item--active");
+    navLink8.classList.remove("navigation__link-btn--active");
+    modBtnWrapper.classList.remove("menu__wrapper--active");
+    callButton.classList.remove("menu__button--active");
+    chatButton.classList.remove("menu__button--active");
+    profileButton.classList.remove("menu__button--active");
+    mailtoLink.classList.remove("menu__mailto--active");
+    calltoLink.classList.remove("menu__callto--active");
+    langWrapper.classList.remove("menu__wrapper--active");
+    langLinkRU.classList.remove("menu__link-btn--active");
+    langLinkEN.classList.remove("menu__link-btn--active");
+    langLinkCH.classList.remove("menu__link-btn--active");
+    menu.classList.remove("menu--active");
+    placeholder.classList.remove("placeholder--active");
+    servicesFirstLineWrapper.classList.remove("services__wrapper--hidden");
+    upenderException.classList.remove("upender__exception--blurred");
+    upenderBlurOverlay.classList.remove("upender__blur-overlay--active");
   } else {
-    burgerButton.classList.add('active');
+    burgerButton.classList.add("menu__button--active");
+    menuLogoWrapper.classList.add("menu__wrapper--active");
+    logoLink.classList.add("menu__logo--active");
+    logoImg.classList.add("logo__img--active");
+    searchButton.classList.add("menu__button--active");
+    navigation.classList.add("menu__navigation--active");
+    navigationList.classList.add("navigation__list--active");
+    navItem1.classList.add("navigation__item--active");
+    navLink1.classList.add("navigation__link-btn--active");
+    navItem2.classList.add("navigation__item--active");
+    navLink2.classList.add("navigation__link-btn--active");
+    navItem3.classList.add("navigation__item--active");
+    navLink3.classList.add("navigation__link-btn--active");
+    navItem4.classList.add("navigation__item--active");
+    navLink4.classList.add("navigation__link-btn--active");
+    navItem5.classList.add("navigation__item--active");
+    navLink5.classList.add("navigation__link-btn--active");
+    navItem6.classList.add("navigation__item--active");
+    navLink6.classList.add("navigation__link-btn--active");
+    navItem7.classList.add("navigation__item--active");
+    navLink7.classList.add("navigation__link-btn--active");
+    navItem8.classList.add("navigation__item--active");
+    navLink8.classList.add("navigation__link-btn--active");
+    modBtnWrapper.classList.add("menu__wrapper--active");
+    callButton.classList.add("menu__button--active");
+    chatButton.classList.add("menu__button--active");
+    profileButton.classList.add("menu__button--active");
+    mailtoLink.classList.add("menu__mailto--active");
+    calltoLink.classList.add("menu__callto--active");
+    langWrapper.classList.add("menu__wrapper--active");
+    langLinkRU.classList.add("menu__link-btn--active");
+    langLinkEN.classList.add("menu__link-btn--active");
+    langLinkCH.classList.add("menu__link-btn--active");
+    menu.classList.add("menu--active");
+    placeholder.classList.add("placeholder--active");
+    servicesFirstLineWrapper.classList.add("services__wrapper--hidden");
+    upenderException.classList.add("upender__exception--blurred");
+    upenderBlurOverlay.classList.add("upender__blur-overlay--active");
+  }
+}
+
+var upenderBlurOverlay = document.getElementById("upenderBlurOverlay");
+upenderBlurOverlay.addEventListener("click", function() {
+  if (upenderBlurOverlay.classList.contains("upender__blur-overlay--active")) {
+    toggleBurgerButton();
   }
 });
+
+var burgerButton = document.getElementById("burgerButton");
+burgerButton.addEventListener("click", toggleBurgerButton);
